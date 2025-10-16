46dd80048490d08f752464507d44bf4750653ed5
giba <georgy.gibizov@gmail.com>
2025-10-16 13:43:27 +0300

    add button with mock card + prikols (gradient etc)

--- app/src/main/java/fi/gibanator/subbearandroid/ui/main/UpcomingCardsScreen.kt
+++ app/src/main/java/fi/gibanator/subbearandroid/ui/main/UpcomingCardsScreen.kt
@@ -2,8 +2,9 @@ package fi.gibanator.subbearandroid.ui.main
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,19 +17,18 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -39,18 +39,32 @@ import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import fi.gibanator.subbearandroid.R
 import fi.gibanator.subbearandroid.data.PeriodUnit
 import fi.gibanator.subbearandroid.data.Subscription
-import kotlinx.coroutines.sync.Mutex
 import java.time.LocalDate
 
 
 @Composable
 fun UpcomingSubscriptionsScreen(
-    vm: UpcomingSubscriptionViewModel = hiltViewModel(),
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    vm: UpcomingSubscriptionViewModel = hiltViewModel()
 ) {
     val subs by vm.subCards.collectAsState()
-    UpcomingSubscriptionsList(subs, {})
-    GradientFab({})
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        UpcomingSubscriptionsList(
+            subs,
+            {},
+            modifier = Modifier.align(Alignment.Center)
+        )
+        GradientFab(
+            { vm.addSubscription() },
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(end = 16.dp, bottom = 16.dp)
+        )
+    }
 }
 @Composable
 fun UpcomingSubscriptionsList(
@@ -77,7 +91,7 @@ fun SubscriptionCard(
         ),
         shape = RoundedCornerShape(12.dp),
         onClick = onClick,
-        modifier = Modifier
+        modifier = modifier
             .padding(6.dp)
             .fillMaxWidth()
     ) {
@@ -108,22 +122,21 @@ fun GradientFab(
     modifier: Modifier = Modifier
 ) {
     val gradient = Brush.linearGradient(
-        colors = listOf(Color(0xFF6A11CB), Color(0xFF2575FC)),
-        start = Offset(0f, 0f),
-        end = Offset(300f, 300f)
+        colors = listOf(Color(0xFF6A11CB), Color(0xFF2575FC), Color(0xBB1427FF))
     )
-    FloatingActionButton(
-        onClick = onClick,
-        containerColor = Color.Transparent,
-        contentColor = Color.White,
+    Box(
+        modifier = modifier
+            .clip(CircleShape)
+            .background(gradient)
+            .clickable(
+                indication = ripple(bounded = false, radius = 40.dp),
+                onClick = onClick,
+                interactionSource = remember { MutableInteractionSource() }
+            )
+            .size(45.dp),
+        contentAlignment = Alignment.Center
     ) {
-        Box(
-            modifier = Modifier
-                .background(brush = gradient, CircleShape)
-
-        ) {
-            Icon(Icons.Filled.Add, contentDescription = "")
-        }
+        Icon(Icons.Filled.Add, contentDescription = "", tint = Color.White)
     }
 }
 
--- app/src/main/java/fi/gibanator/subbearandroid/ui/main/UpcomingSubscriptionViewModel.kt
+++ app/src/main/java/fi/gibanator/subbearandroid/ui/main/UpcomingSubscriptionViewModel.kt
@@ -3,6 +3,7 @@ package fi.gibanator.subbearandroid.ui.main
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import fi.gibanator.subbearandroid.data.PeriodUnit
 import fi.gibanator.subbearandroid.data.Subscription
 import fi.gibanator.subbearandroid.data.SubscriptionRepo
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -10,6 +11,7 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import java.time.LocalDate
 import javax.inject.Inject
 
 @HiltViewModel
@@ -31,10 +33,16 @@ class UpcomingSubscriptionViewModel @Inject constructor(
         }
     }
 
-    fun addSubscription(subscription: Subscription) {
-        viewModelScope.launch { 
-            repo.add(subscription)
-            _subCards.update { it + subscription }
+    fun addSubscription() {
+        viewModelScope.launch {
+            val sub = Subscription(name = "aaa",
+                logoUri = "",
+                periodUnit = PeriodUnit.DAY,
+                unitCount = 1,
+                startDate = LocalDate.now(),
+                nextCharge = LocalDate.now())
+            repo.add(sub)
+            _subCards.update { it + sub }
         }
     }
 }
